e3d425d9c6f1a94b394b3e7e9dd12c5756d481d3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-05-01 11:58:29 +0000

    Deploy TeamLimoncello/limoncelloHome to github.com/TeamLimoncello/limoncelloHome.git:gh-pages

--- script/main.js
+++ script/main.js
@@ -1,3 +1,8 @@
 loaded.then(() => {
-    $('#sidebar-button').click(() => $('body').toggleClass('sidebar-open sidebar-closed'));
+    $('#sidebar-button').click(() => $(document.body).toggleClass('sidebar-open sidebar-closed'));
+
+    $(document.body).on('click', '.close-sidebar', () => {
+        $(document.body).addClass('sidebar-closed').removeClass('sidebar-open');
+        return false;
+    });
 });
